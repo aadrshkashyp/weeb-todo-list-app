73f6be3cb055dc556d508ee638534c5eea9e6533
Aadarshkashyap51 <aadarshkashyap51@gmail.com>
2021-08-01 13:32:31 +0530

    css issue fixing

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,27 +1,58 @@
-import React from 'react';
-import {GrSuse, } from 'react-icons/gr';
-import {AiOutlinePlus, } from 'react-icons/ai';
+import React, { useState } from 'react';
+import { FaPizzaSlice } from 'react-icons/fa';
+import PropTypes from 'prop-types';
+import { AddTask } from '../AddTask';
 
-export const Header = () => {
+export const Header = ({ darkMode, setDarkMode }) => {
+  const [shouldShowMain, setShouldShowMain] = useState(false);
+  const [showQuickAddTask, setShowQuickAddTask] = useState(false);
 
-    return <header className="header" data-testid="header">
-        <nav>
-            <div className="logo">
-                <img src="/images/logo.png" alt="logo" />
-            </div>
-            <div className="settings">
-                <ul>
-                    <li>
-                        <AiOutlinePlus />
-                    </li>
-                    <li>
-                        <GrSuse />
-                    </li>
-                </ul>
-            </div>
-        </nav>
+  return (
+    <header className="header" data-testid="header">
+      <nav>
+        <div className="logo">
+          <img src="/images/logo.png" alt="Todoist" />
+        </div>
+        <div className="settings">
+          <ul>
+            <li className="settings__add">
+              <button
+                data-testid="quick-add-task-action"
+                aria-label="Quick add task"
+                type="button"
+                onClick={() => {
+                  setShowQuickAddTask(true);
+                  setShouldShowMain(true);
+                }}
+              >
+                +
+              </button>
+            </li>
+            <li className="settings__darkmode">
+              <button
+                data-testid="dark-mode-action"
+                aria-label="Darkmode on/off"
+                type="button"
+                onClick={() => setDarkMode(!darkMode)}
+              >
+                <FaPizzaSlice />
+              </button>
+            </li>
+          </ul>
+        </div>
+      </nav>
 
-    </header>;
+      <AddTask
+        showAddTaskMain={false}
+        shouldShowMain={shouldShowMain}
+        showQuickAddTask={showQuickAddTask}
+        setShowQuickAddTask={setShowQuickAddTask}
+      />
+    </header>
+  );
+};
 
-
+Header.propTypes = {
+  darkMode: PropTypes.bool.isRequired,
+  setDarkMode: PropTypes.func.isRequired,
 };
--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,43 +1,122 @@
-import React from 'react';
-import {HiOutlineInboxIn, } from 'react-icons/hi';
-import {IoTodayOutline, } from 'react-icons/io5';
-import {BiCalendarWeek, BiChevronDownCircle, } from 'react-icons/bi';
+import React, { useState } from 'react';
+import {
+  FaChevronDown,
+  FaInbox,
+  FaRegCalendarAlt,
+  FaRegCalendar,
+} from 'react-icons/fa';
+import { Projects } from '../Projects';
+import { useSelectedProjectValue } from '../../context';
+import { AddProject } from '../AddProject';
 
-export const Sidebar = () => (
+export const Sidebar = () => {
+  const { setSelectedProject } = useSelectedProjectValue();
+  const [active, setActive] = useState('inbox');
+  const [showProjects, setShowProjects] = useState(true);
 
-<div className="sidebar" data-testid="sidebar">
-    <ul className="sidebar__generic">
-        <li>
+  return (
+    <div className="sidebar" data-testid="sidebar">
+      <ul className="sidebar__generic">
+        <li
+          data-testid="inbox"
+          className={active === 'inbox' ? 'active' : undefined}
+        >
+          <div
+            data-testid="inbox-action"
+            aria-label="Show inbox tasks"
+            tabIndex={0}
+            role="button"
+            onClick={() => {
+              setActive('inbox');
+              setSelectedProject('INBOX');
+            }}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                setActive('inbox');
+                setSelectedProject('INBOX');
+              }
+            }}
+          >
             <span>
-                <HiOutlineInboxIn />
+              <FaInbox />
             </span>
             <span>Inbox</span>
+          </div>
         </li>
-
-        <li>
+        <li
+          data-testid="today"
+          className={active === 'today' ? 'active' : undefined}
+        >
+          <div
+            data-testid="today-action"
+            aria-label="Show today's tasks"
+            tabIndex={0}
+            role="button"
+            onClick={() => {
+              setActive('today');
+              setSelectedProject('TODAY');
+            }}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                setActive('today');
+                setSelectedProject('TODAY');
+              }
+            }}
+          >
             <span>
-                <IoTodayOutline />
+              <FaRegCalendar />
             </span>
             <span>Today</span>
+          </div>
         </li>
-
-        <li>
+        <li
+          data-testid="next_7"
+          className={active === 'next_7' ? 'active' : undefined}
+        >
+          <div
+            data-testid="next_7-action"
+            aria-label="Show tasks for the next 7 days"
+            tabIndex={0}
+            role="button"
+            onClick={() => {
+              setActive('next_7');
+              setSelectedProject('NEXT_7');
+            }}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                setActive('next_7');
+                setSelectedProject('NEXT_7');
+              }
+            }}
+          >
             <span>
-                <BiCalendarWeek />
+              <FaRegCalendarAlt />
             </span>
-            <span>Next 7 Days</span>
+            <span>Next 7 days</span>
+          </div>
         </li>
-    </ul>
-
-    <div className="sidebar__middle">
+      </ul>
+      <div
+        className="sidebar__middle"
+        aria-label="Show/hide projects"
+        onClick={() => setShowProjects(!showProjects)}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter') setShowProjects(!showProjects);
+        }}
+        role="button"
+        tabIndex={0}
+      >
         <span>
-            <BiChevronDownCircle />
+          <FaChevronDown
+            className={!showProjects ? 'hidden-projects' : undefined}
+          />
         </span>
         <h2>Projects</h2>
+      </div>
+
+      <ul className="sidebar__projects">{showProjects && <Projects />}</ul>
+
+      {showProjects && <AddProject />}
     </div>
-
-     <ul className="sidebar__projects">Projects will be here!</ul>
-     Add Projects Here!!!
-
-</div>
-);
+  );
+};
